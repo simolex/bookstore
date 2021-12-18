24024249aacab6b33fd45cab0feb939a5137c861
simolex <simolex@yandex.ru>
2021-12-18 16:11:46 +0300

    media responsive: category& unreleased

--- js/main.js
+++ js/main.js
@@ -1,21 +1,102 @@
-const hotelSlider = new Swiper(".categories__line", {
+const categorySlider = new Swiper(".categories__line", {
   loop: false,
 
-  //   keyboard: {
-  //     enabled: true,
-  //     onlyInViewport: true,
-  //   },
-  slidesPerView: 4,
-  spaceBetween: 26,
   wrapperClass: "categories-wrapper",
   slideClass: "category",
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
   navigation: {
-    nextEl: ".swiper-button-prev",
-    prevEl: ".swiper-button-next",
+    nextEl: ".category__button--next",
+    prevEl: ".category__button--prev",
+    disabledClass: "category__button--disabled",
+  },
+
+  slidesPerView: 4,
+  spaceBetween: 26,
+  breakpointsBase: "container",
+
+  breakpoints: {
+    // when window width is >= 320px
+    278: {
+      slidesPerView: 2,
+      slidesPerColumn: 2,
+      grid: {
+        rows: 2,
+        fill: "row",
+      },
+      spaceBetween: 9,
+    },
+    574: {
+      slidesPerView: 2,
+      slidesPerColumn: 2,
+      grid: {
+        rows: 2,
+        fill: "row",
+      },
+      spaceBetween: 9,
+    },
+    698: {
+      slidesPerView: 2,
+      slidesPerColumn: 2,
+      grid: {
+        rows: 1,
+        //fill: "column",
+      },
+      spaceBetween: 19,
+    },
+    958: {
+      slidesPerView: 3,
+      spaceBetween: 29,
+      grid: {
+        rows: 1,
+      },
+    },
+    1138: {
+      slidesPerView: 4,
+      spaceBetween: 29,
+      grid: {
+        rows: 1,
+      },
+    },
   },
 });
-//hotelSlider();
+
+const unreleasedSlider = new Swiper(".unreleased__slider", {
+  loop: false,
+
+  wrapperClass: "unreleased__vitrine",
+  slideClass: "unreleased__item",
+  navigation: {
+    nextEl: ".unreleased__arrow--right",
+    prevEl: ".unreleased__arrow--left",
+    disabledClass: "unreleased__arrow--disabled",
+    //lockClass: "unreleased__arrow--disabled",
+  },
+
+  slidesPerView: 4,
+  spaceBetween: 26,
+  breakpointsBase: "container",
+
+  breakpoints: {
+    // when window width is >= 320px
+    200: {
+      slidesPerView: 1,
+
+      spaceBetween: 0,
+    },
+    519: {
+      slidesPerView: 2,
+      spaceBetween: 160,
+    },
+    698: {
+      slidesPerView: 3,
+      spaceBetween: 42,
+    },
+    958: {
+      slidesPerView: 4,
+      spaceBetween: 45,
+    },
+    1138: {
+      slidesPerView: 5,
+      spaceBetween: 29,
+    },
+  },
+});
