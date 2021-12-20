7bf046d173687186e62eb917870f5826d66f1666
simolex <simolex@yandex.ru>
2021-12-20 10:05:28 +0300

    fix: smoth scroll for PopUp

--- js/smothScroll.js
+++ js/smothScroll.js
@@ -1,11 +1,16 @@
 const smothScroll = () => {
   const linksHead = document.querySelectorAll(".navbar-menu__link");
+  const popUpScroll = document.querySelector(".pop-up__button");
+  const linkArray = [...linksHead, popUpScroll];
 
-  linksHead.forEach((link) => {
+  linkArray.forEach((link) => {
     link.addEventListener("click", (event) => {
       event.preventDefault();
-
-      const ID = event.target.getAttribute("href").substr(1);
+      //if()
+      const ID =
+        event.target.tagName.toLowerCase() == "a"
+          ? event.target.getAttribute("href").substr(1)
+          : event.target.closest("a").getAttribute("href").substr(1);
 
       document.getElementById(ID).scrollIntoView({
         behavior: "smooth",
